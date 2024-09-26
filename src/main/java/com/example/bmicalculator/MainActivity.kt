9874c73fa727d8ca87b4d61e39b1dc083155bac1
Taha-Sid <74993189+Taha-Sid@users.noreply.github.com>
2024-09-26 20:06:14 +0100

    Add files via upload
    Calculated bmi and returned result

--- src/main/java/com/example/bmicalculator/MainActivity.kt
+++ src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -22,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         binding.heightPicker.minValue = 100
         binding.heightPicker.maxValue = 250
 
+        binding.weightPicker.setOnValueChangedListener{_,_,_ ->
+            calculateBMI()
+        }
+
+        binding.heightPicker.setOnValueChangedListener{_,_,_ ->
+            calculateBMI()
+        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -30,4 +38,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    private fun calculateBMI() {
+        val height = binding.heightPicker.value
+        val doubleHeight = height.toDouble() / 100
+
+        val weight = binding.weightPicker.value
+
+        val bmi = weight.toDouble() / (height * doubleHeight)
+
+        binding.resultsTV.text = String.format("Your BMI is: %.2f", bmi)
+        binding.healthyTV.text = String.format("Considered: %.2f", healthyMessage(bmi))
+    }
+
+    private fun healthyMessage(bmi: Double): String {
+        if(bmi < 18.5)
+            return "Underweight"
+        if(bmi < 25.0)
+            return "Healthy"
+        if(bmi < 30.0)
+            return "Overweight"
+
+        return "Obese"
+    }
+
 }
